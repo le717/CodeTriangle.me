604154909ad7404461411a720e43f16fb65f2c34
Caleb Ely <le717.code@yahoo.com>
2016-02-27 17:28:18 -0500

    Integrate Autoprefixer into SCSS build script

--- scripts/build-scss.js
+++ scripts/build-scss.js
@@ -1,9 +1,11 @@
 /* global process */
 "use strict";
 
-const fs   = require("fs"),
-      path = require("path"),
-      sass = require("node-sass"),
+const fs           = require("fs"),
+      path         = require("path"),
+      sass         = require("node-sass"),
+      postcss      = require("postcss"),
+      autoprefixer = require("autoprefixer"),
 
       // Root project directory
       rootDir = process.cwd(),
@@ -12,27 +14,52 @@ const fs   = require("fs"),
       output     = require("./scss-build-details"),
       outputPath = path.join(rootDir, "build", "css", output.details.outputFile);
 
+
+/**
+ * Vendor-prefix the CSS using Autoprefixer.
+ *
+ * @param {string} css - CSS to be prefixed.
+ */
+function prefixCSS(css) {
+  return postcss([autoprefixer]).process(css);
+}
+
+/**
+ * Write the CSS to file.
+ *
+ * @param {string} css - CSS to be written.
+ */
+function saveCSS(css) {
+    fs.writeFile(outputPath, css, (err) => {
+    if (err) {
+      console.error("Unable to build CSS.");
+      throw new Error(err);
+    }
+    console.log("CSS built successfully.");
+  });
+}
+
 // Switch to where we keep out scripts
 process.chdir(path.join(rootDir, "scss"));
 
 // Compile the SCSS
 sass.render({
   file: output.details.inputFile,
-  outputStyle: output.details.style,
+  outputStyle: output.details.scssStyle,
   indentedSyntax: output.details.indented
 
 }, (err, result) => {
   // There was an error generating the CSS
   if (err) throw new Error(err);
-  console.log(result.css);
 
-  // Write it to file
-  fs.writeFile(outputPath, result.css, (err) => {
-    if (err) {
-      console.error("Unable to build CSS.");
-      throw new Error(err);
-    }
+  // Run the CSS through Autoprefixer
+  prefixCSS(result.css)
+  .then((result) => {
+    result.warnings().forEach((warn) => {
+        console.warn(warn.toString());
+    });
 
-    console.log("CSS built successfully.");
+    // Write it to disk
+    saveCSS(result.css);
   });
 });
