60e3f80416de92c8a05511e48b3efea3e6bc6bc7
Caleb Ely <le717.code@yahoo.com>
2016-07-25 22:01:02 -0400

    Minor project box fixes

--- js/project-box.js
+++ js/project-box.js
@@ -4,18 +4,25 @@
   "use strict";
   var qProjectContainer = document.querySelector("#projects-container");
 
-  function findParentEle(ele, classEle) {
-    // We found the desired element
-    if (ele.classList.contains(classEle)) {
-      return ele;
-    } else {
-      return findParentEle(ele.parentElement, classEle);
-    }
-  }
+  function findParent(ele, _class) {
+		// The desired element was not found on the page
+		if (ele === null) {
+			return null;
+		}
+
+		// We found the desired element
+		if (ele.classList.contains(_class)) {
+			return ele;
+
+			// Keep searching for the element
+		} else {
+			return findParent(ele.parentElement, _class);
+		}
+	 }
 
   qProjectContainer.addEventListener("click", function(e) {
     // Find the project box
-    var qProjectBox = findParentEle(e.target, "project-box");
+    var qProjectBox = findParent(e.target, "project-box");
 
     // Hide/show the clicked box
     if (qProjectBox.classList.contains("project-box")) {
