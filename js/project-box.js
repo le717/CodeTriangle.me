85c273a5e993b7c8cb689629c5ae28b07f5a3669
Caleb Ely <le717.code@yahoo.com>
2017-07-29 11:20:49 -0400

    Convert file to spaces

--- js/project-box.js
+++ js/project-box.js
@@ -1,24 +1,22 @@
-/* jshint browser: true */
-
 (function() {
   "use strict";
   var qProjectContainer = document.querySelector("#projects-container");
 
   function findParent(ele, _class) {
-		// The desired element was not found on the page
-		if (ele === null) {
-			return null;
-		}
+    // The desired element was not found on the page
+    if (ele === null) {
+      return null;
+    }
 
-		// We found the desired element
-		if (ele.classList.contains(_class)) {
-			return ele;
+    // We found the desired element
+    if (ele.classList.contains(_class)) {
+      return ele;
 
-			// Keep searching for the element
-		} else {
-			return findParent(ele.parentElement, _class);
-		}
-	 }
+      // Keep searching for the element
+    } else {
+      return findParent(ele.parentElement, _class);
+    }
+   }
 
   qProjectContainer.addEventListener("click", function(e) {
     // Find the project box
