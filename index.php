cda5e1f2075555aee3f53d7597cadd86f9d62497
Caleb Ely <le717.code@yahoo.com>
2017-07-29 11:14:08 -0400

    + Added rel="noopener noreferrer"
    * Updated social media <meta> tags

--- index.php
+++ index.php
@@ -4,9 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="theme-color" content="#3D3D3D">
-  <link rel="apple-touch-icon-precomposed" sizes="192x192" href="apple-touch-icon-precomposed.png">
-  <link rel="shortcut icon" sizes="192x192" href="favicon.png">
+
   <?= file_get_contents('_includes/social-tags.html'); ?>
   <!--[if IE]>
     <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
@@ -69,7 +67,7 @@
             <h3 class="title">CarDoozy Store</h3>
             <p class="date">April - July 2016</p>
             <div class="desc">
-              <p>Developed, managed, and maintained the <a target="_blank" href="http://www.cardoozy.com/">CarDoozy Store</a>, an e-commerce site targeted towards car dealers to provide state-required printed forms and commonly used dealer products. Work included upgrading codebase from PHP 5.3 to PHP 5.6, implementing new features, fixing, removing, or rewriting broken, incomplete, or poorly designed features, and composing a road map of future developments.</p>
+              <p>Developed, managed, and maintained the <a target="_blank" rel="noopener noreferrer" href="http://www.cardoozy.com/">CarDoozy Store</a>, an e-commerce site targeted towards car dealers to provide state-required printed forms and commonly used dealer products. Work included upgrading codebase from PHP 5.3 to PHP 5.6, implementing new features, fixing, removing, or rewriting broken, incomplete, or poorly designed features, and composing a road map of future developments.</p>
             </div>
           </aside>
 
@@ -77,7 +75,7 @@
             <h3 class="title">CarDoozy.com</h3>
             <p class="date">May 2016</p>
             <div class="desc">
-              <p>Designed, implemented, and launched <a target="_blank" href="http://www.cardoozy.com/">CarDoozy.com</a>, the promotional site for an online store geared towards saving automotive dealer associations time and money on dealer product, promotional, and printed needs. Pre-existing logo and stock photo used to create design scheme.</p>
+              <p>Designed, implemented, and launched <a target="_blank" rel="noopener noreferrer" href="http://www.cardoozy.com/">CarDoozy.com</a>, the promotional site for an online store geared towards saving automotive dealer associations time and money on dealer product, promotional, and printed needs. Pre-existing logo and stock photo used to create design scheme.</p>
             </div>
           </aside>
 
@@ -85,7 +83,7 @@
             <h3 class="title">CodeTriangle</h3>
             <p class="date">February 2016</p>
             <div class="desc">
-              <p>My personal site, which you are currently browsing. Registered in July 2015, the current design iteration (and yes, it is <a target="_blank" href="https://github.com/le717/CodeTriangle">open-source</a>) was published on February 29, 2016.</p>
+              <p>My personal site, which you are currently browsing. Registered in July 2015, the current design iteration (and yes, it is <a target="_blank" rel="noopener noreferrer" href="https://github.com/le717/CodeTriangle">open-source</a>) was published on February 29, 2016.</p>
             </div>
           </aside>
 
@@ -101,7 +99,7 @@
             <h3 class="title">Notepad.exe</h3>
             <p class="date">October 2015</p>
             <div class="desc">
-              <p>As an afternoon project, I recreated <a target="_blank" href="https://en.wikipedia.org/wiki/Notepad_%28software%29">Windows Notepad</a> in JavaScript, implementing file creation, saving, and printing, as well as the notorous (disabled by default!) word-wrapping. Both a <a href="http://notepad.codetri.net">live demo</a> and <a target="_blank" href="https://github.com/le717/notepad.exe">source code</a> are available.</p>
+              <p>As an afternoon project, I recreated <a target="_blank" rel="noopener noreferrer" href="https://en.wikipedia.org/wiki/Notepad_%28software%29">Windows Notepad</a> in JavaScript, implementing file creation, saving, and printing, as well as the notorous (disabled by default!) word-wrapping. Both a <a href="http://notepad.codetri.net">live demo</a> and <a target="_blank" rel="noopener noreferrer" href="https://github.com/le717/notepad.exe">source code</a> are available.</p>
             </div>
           </aside>
 
@@ -109,7 +107,7 @@
             <h3 class="title">Model Worlds</h3>
             <p class="date">July 2015</p>
             <div class="desc">
-              <p>I recreated the Steam Workshop-inspired <a target="_blank" href="http://web.archive.org/web/20160121032229/http://www.modelworlds.net/">Model Worlds</a> fan site for the <a target="_blank" href="http://store.steampowered.com/app/332310">LEGO&reg; Worlds</a> video game by <a target="_blank" href="http://www.ttgames.com/">TT Games</a>. In one week I recreated a modified, responsive design of the site and wrote a basic, but usable, user account system in PHP. Due to unforseen circumstances and side-project status, the site was never completed and eventually <a target="_blank" href="https://github.com/le717/Model-Worlds">open-sourced</a>.</p>
+              <p>I recreated the Steam Workshop-inspired <a target="_blank" rel="noopener noreferrer" href="http://web.archive.org/web/20160121032229/http://www.modelworlds.net/">Model Worlds</a> fan site for the <a target="_blank" rel="noopener noreferrer" href="http://store.steampowered.com/app/332310">LEGO&reg; Worlds</a> video game by <a target="_blank" rel="noopener noreferrer" href="http://www.ttgames.com/">TT Games</a>. In one week I recreated a modified, responsive design of the site and wrote a basic, but usable, user account system in PHP. Due to unforseen circumstances and side-project status, the site was never completed and eventually <a target="_blank" rel="noopener noreferrer" href="https://github.com/le717/Model-Worlds">open-sourced</a>.</p>
             </div>
           </aside>
 
@@ -117,7 +115,7 @@
             <h3 class="title">HTML Skeleton</h3>
             <p class="date">January 2014</p>
             <div class="desc">
-              <p>Initially developed to eliminate redundant HTML typing in class, it was later expanded to insert commonly used HTML snippets. Since <a target="_blank" href="https://github.com/le717/brackets-html-skeleton">open-sourcing</a> and uploading to the extension registry, this small, occasionally updated <a target="_blank" href="http://brackets.io">Brackets</a> extension has been downloaded over 85,000 times with 9,600 active installations.</p>
+              <p>Initially developed to eliminate redundant HTML typing in class, it was later expanded to insert commonly used HTML snippets. Since <a target="_blank" rel="noopener noreferrer" href="https://github.com/le717/brackets-html-skeleton">open-sourcing</a> and uploading to the extension registry, this small, occasionally updated <a target="_blank" rel="noopener noreferrer" href="http://brackets.io">Brackets</a> extension has been downloaded over 85,000 times with 9,600 active installations.</p>
             </div>
           </aside>
         </div>
@@ -131,7 +129,7 @@
 
         <p>I have a small blog where I predominantly write about Website design, development, and general technology. A strong supporter of open source software, I believe some of the best software currently available is open source. That is why I both actively use and contribute to open source projects as well as open source the majority of my own work.</p>
 
-        <p>I began programming in January 2013 by teaching myself Python, and in September I enrolled at <a target="_blank" href="http://southeasterntech.edu/">Southeastern Technical College</a>, obtaining my Associates of Applied Science in Website Development and graduating with honors in December 2015. I am currently enrolled at <a target="_blank" href="http://ega.edu">East Georgia State College</a> working on my Bachelor's degree.</p>
+        <p>I began programming in January 2013 by teaching myself Python, and in September I enrolled at <a target="_blank" rel="noopener noreferrer" href="http://southeasterntech.edu/">Southeastern Technical College</a>, obtaining my Associates of Applied Science in Website Development and graduating with honors in December 2015. I am currently enrolled at <a target="_blank" rel="noopener noreferrer" href="http://ega.edu">East Georgia State College</a> working on my Bachelor's degree.</p>
 
         <blockquote>To me, programming is not about trying to write something that will be picked up by some big corporation and bought for lots of money. Instead, it is about doing my job in the most excellent way possible and making the product work so well that users never have to think about how or why it works, it just works.</blockquote>
       </div>
@@ -143,17 +141,17 @@
         <p class="dropcap">If you have any questions, comments, or simply want to say hi, you can drop me a line! I need many more words here to add at least two or three more sentences. Having only one sentence for this large section is not at all good. Without them it feels rather... lacking and empty.</p>
 
         <div class="social-icons">
-          <a target="_blank" href="https://twitter.com/cely717">
+          <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/cely717">
             <div class="icon fi-social-twitter"></div>
             <span class="text">Twitter</span>
           </a>
 
-          <a target="_blank" href="https://github.com/le717">
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/le717">
             <div class="icon fi-social-github"></div>
             <span class="text">GitHub</span>
           </a>
 
-          <a target="_blank" href="https://medium.com/@cely717">
+          <a target="_blank" rel="noopener noreferrer" href="https://medium.com/@cely717">
             <div class="icon fi-book-bookmark"></div>
             <span class="text">Blog</span>
           </a>
